003e8d72e675e62fbd6a28c0c7385302b0c1a960
Onkar Chougule <onkarchougule84@gmail.com>
2026-07-03 01:09:49 +0530

    fix(health): make health routes version neutral

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -2,30 +2,34 @@ import { Controller, Get, VERSION_NEUTRAL, Version } from '@nestjs/common';
 import { HealthService } from './health.service';
 
 @Controller()
-@Version(['1', VERSION_NEUTRAL])
 export class HealthController {
   constructor(private readonly health: HealthService) {}
 
+  @Version(VERSION_NEUTRAL)
   @Get('health')
   healthCheck() {
     return this.health.health();
   }
 
+  @Version(VERSION_NEUTRAL)
   @Get('ready')
   readiness() {
     return this.health.ready();
   }
 
+  @Version(VERSION_NEUTRAL)
   @Get('version')
   version() {
     return this.health.version();
   }
 
+  @Version(VERSION_NEUTRAL)
   @Get('database')
   database() {
     return this.health.database();
   }
 
+  @Version(VERSION_NEUTRAL)
   @Get('redis')
   redis() {
     return this.health.redis();
